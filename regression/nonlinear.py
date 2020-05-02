ed5c5c53b1b6763136f85cada75b375fb2153756
goku <coding.game@gmail.com>
2020-05-02 16:58:47 -0400

    Add Polynomial feature class

---
+++ regression/nonlinear.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+class PolynomialFeatures:
+  def __init__(self, degree):
+    self.degree = degree
+
+  def fit(self, X, y=None):
+
+  def tranform(self, X):
+
+
