2e45ca7554f85fe796e90555a909fc60955e6835
YvesHarrison <yveslee15@gmail.com>
2020-05-03 13:55:25 +0800

    two feature training test and numpy matrix overflow

--- regression/tests/lineartest.py
+++ regression/tests/lineartest.py
@@ -14,6 +14,10 @@ with open('../../train.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     x = np.array([int(row['sqft_living']) for row in reader])
 
+with open('../../train.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    x1 = np.array([int(row['sqft_above']) for row in reader])
+
 with open('../../test.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     yt = np.array([int(row['price']) for row in reader])
@@ -21,17 +25,28 @@ with open('../../test.csv') as csvfile:
 with open('../../test.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     xt = np.array([int(row['sqft_living']) for row in reader])
+
+with open('../../test.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    xt1 = np.array([int(row['sqft_above']) for row in reader])
+
 m = len(x)
+# x = np.hstack([x, x1])
 x = x.reshape(m,1)
+x1 = x1.reshape(m,1)
 y = y.reshape(m,1)
+x = np.hstack([x, x1])
+print(x)
 csvfile.close()
 
 n = len(xt)
 xt = xt.reshape(n,1)
+xt1 = xt1.reshape(n,1)
 yt = yt.reshape(n,1)
+xt = np.hstack([xt, xt1])
+print(xt)
 csvfile.close()
-
-LR = linear.LinearRegression(0.0000001, 100)
+LR = linear.LinearRegression(0.0000001, 10)
 LR.fit(x, y)
 print(LR.coef, LR.intercept)
 y_pred = LR.predict(xt)
