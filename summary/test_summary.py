ad63d9a4745c3eb9cd1064fee65ec1fb75fd1ace
goku <coding.game@gmail.com>
2020-05-06 16:00:49 -0400

    Add summary

---
+++ summary/test_summary.py
@@ -0,0 +1,16 @@
+import unittest
+
+from summary import Summary
+
+class TestSummary(unittest.TestCase):
+    def testSummary(self):
+        s = Summary()
+        s.setTitle("Test title")
+        s.append("aa", 123)
+        s.append("bb", 234)
+        res = s.get_summary()
+        self.assertIsNotNone(res)
+
+
+if __name__ == '__main__':
+  unittest.main()
